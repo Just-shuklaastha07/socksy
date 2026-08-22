0adf9f5fcc12dc5b747b3e04a615c8fa610b453b
Just-shuklaastha07 <shuklaastha07@gmail.com>
2026-08-22 17:35:05 +0530

    feat: connect backend with MongoDB

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const productRoutes = require("./routes/productRoutes");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Socksy API 🧦",
+  });
+});
+
+app.use("/api/products", productRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected ✅");
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Socksy server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection failed ❌");
+    console.error(error);
+  });
